53ffb3da1f7b6bde643ba25c863c7f5aa97e6613
harperslone <harperslone@me.com>
2026-01-01 19:53:39 +0100

    update

--- app/next.config.ts
+++ app/next.config.ts
@@ -9,14 +9,38 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config) => {
+  // Exclude Sanity CLI from being processed
+  serverExternalPackages: ['@sanity/cli'],
+  // Exclude from file tracing
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/@sanity/cli/**/*',
+    ],
+  },
+  webpack: (config, { isServer }) => {
     // Ignore Sanity CLI template files that shouldn't be processed
     const webpack = require('webpack');
+    
+    // Add IgnorePlugin to exclude template files from all contexts
     config.plugins.push(
       new webpack.IgnorePlugin({
         resourceRegExp: /@sanity\/cli\/templates/,
       })
     );
+    
+    // Exclude from module resolution - make it resolve to false/null
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@sanity/cli/templates': false,
+    };
+    
+    // Add a rule to ignore these files completely (both server and client)
+    config.module.rules.push({
+      test: /node_modules\/@sanity\/cli\/templates\/.*\.ts$/,
+      use: 'ignore-loader',
+      enforce: 'pre', // Apply before other loaders
+    });
+    
     return config;
   },
 };
