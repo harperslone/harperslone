bc25f0f74b054f516887a832a8b151794b74e07a
harperslone <harperslone@me.com>
2026-01-01 19:50:53 +0100

    updqte

--- app/next.config.ts
+++ app/next.config.ts
@@ -9,6 +9,16 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    // Ignore Sanity CLI template files that shouldn't be processed
+    const webpack = require('webpack');
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /@sanity\/cli\/templates/,
+      })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
